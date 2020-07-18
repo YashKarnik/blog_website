27aa86593b42b5c73c75846faa6d68fb1007c7d2
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2020-07-18 23:23:39 +0530

    copyright added

--- client/src/components/footer.component.jsx
+++ client/src/components/footer.component.jsx
@@ -4,7 +4,7 @@ export default function footerComponent() {
   const year = new Date().getFullYear()
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright ⓒ {year} | Yash Karnik</p>
     </footer>
   )
 }
